2762d55ea4ac27ca508b7a185b9d20e8c0c7598c
Arnold Vásquez <arnoldev@disroot.org>
2022-10-10 03:03:19 -0300

    Martingala system was added and completed

--- ruleta.sh
+++ ruleta.sh
@@ -36,3 +36,88 @@ done
 if [ ! "$1" ]; then
   helpPanel
 fi
+
+martingala ()
+{
+  echo -e "\n${yellowColour}[+] ${endColour}${grayColour}Dinero actual: ${endColour}${yellowColour}$money${endColour}\$\n"
+  echo -ne "${yellowColour}[+] ${endColour}${grayColour}¿Cuál es la cantidad a apostar? -> ${endColour}" && read initial_bet
+  echo -ne "${yellowColour}[+] ${endColour}${grayColour}¿A qué deseas apostar continuamente (par/impar)? -> ${endColour}" && read par_impar
+# Ocultar el cursor
+tput civis 
+backup_initial_bet=$initial_bet
+play_counter=0
+bad_plays=""
+while true; do
+  money="$(($money-$initial_bet))"
+  #echo -e "\n${yellowColour}[+] ${endColour}Acabas de apostar${blueColour} $initial_bet${endColour}\$ y en total tienes${yellowColour} $money${endColour}\$"
+  random_number="$(($RANDOM % 37))"
+  if [ $money -ge 0 ]; then
+    case "$par_impar" in
+      par)
+        if [ "$random_number" -eq 0 ]; then
+         #echo -e "${yellowColour}[+] ${endColour}${redColour}Ha salido${endColour}${blueColour} 0${endColour}${redColour}, por lo tanto has perdido${endColour}"
+          initial_bet=$((initial_bet*2))
+          bad_plays+="$random_number "
+          #echo -e "${yellowColour}[+] ${endColour}Tu saldo es: ${blueColour}$money\$${endColour}"
+        else
+          #echo -e "${yellowColour}[+] ${endColour}Ha salido el número${blueColour} $random_number${endColour}"
+          if [[ "(($random_number % 2))" -eq 0 ]]; then
+            #echo -e "${yellowColour}[+] ${endColour}${greenColour}El número que ha salido es par${endColour}"
+            reward="$(($initial_bet*2))"
+            money="$(($money+$reward))"
+            bad_plays=""
+            #echo -e "${yellowColour}[+] ${endColour}¡Has ganado${greenColour} $reward${endColour}\$!, tu nuevo saldo es:${blueColour} $money${endColour}\$"
+            initial_bet=$backup_initial_bet
+          elif [[ "(($random_number % 2))" -ne 0 ]]; then
+            #echo -e "${yellowColour}[+] ${endColour}${redColour}El número que ha salido es impar${endColour}"
+            initial_bet=$((initial_bet*2))
+            bad_plays+="$random_number "
+            #echo -e "${yellowColour}[+] ${endColour}Tu saldo es: ${blueColour}$money\$${endColour}"
+          fi
+        fi
+      ;;
+      impar)
+        if [ "$random_number" -eq 0 ]; then
+         #echo -e "${yellowColour}[+] ${endColour}${redColour}Ha salido${endColour}${blueColour} 0${endColour}${redColour}, por lo tanto has perdido${endColour}"
+          initial_bet=$((initial_bet*2))
+          bad_plays+="$random_number "
+          #echo -e "${yellowColour}[+] ${endColour}Tu saldo es: ${blueColour}$money\$${endColour}"
+        else
+          #echo -e "${yellowColour}[+] ${endColour}Ha salido el número${blueColour} $random_number${endColour}"
+          if [[ ! "(($random_number % 2))" -eq 0 ]]; then
+            #echo -e "${yellowColour}[+] ${endColour}${greenColour}El número que ha salido es impar${endColour}"
+            reward="$(($initial_bet*2))"
+            money="$(($money+$reward))"
+            bad_plays=""
+            #echo -e "${yellowColour}[+] ${endColour}¡Has ganado${greenColour} $reward${endColour}\$!, tu nuevo saldo es:${blueColour} $money${endColour}\$"
+            initial_bet=$backup_initial_bet
+          elif [[ "(($random_number % 2))" -eq 0 ]]; then
+            #echo -e "${yellowColour}[+] ${endColour}${redColour}El número que ha salido es par${endColour}"
+            initial_bet=$((initial_bet*2))
+            bad_plays+="$random_number "
+            #echo -e "${yellowColour}[+] ${endColour}Tu saldo es: ${blueColour}$money\$${endColour}"
+          fi
+        fi
+      ;;
+    esac
+  else
+    echo -e "\n${yellowColour}[!]${endColour}${redColour} Te has quedado sin saldo${endColour}"
+    echo -e "\t${yellowColour}[+]${endColour} Ha habido un total de ${yellowColour}$play_counter${endColour} jugada(s)"
+    echo -e "\t${yellowColour}[+]${endColour} A continuación se representarán las malas jugadas concecutivas que provocaron tu derrota:"
+    echo -e "\t${redColour}$bad_plays${endColour}"
+    tput cnorm; exit 0
+  fi
+
+  tput cnorm
+  let play_counter+=1
+done
+}
+
+if [ $money ] && [ $technique ]; then
+  if [ $technique -eq 1 ]; then
+    martingala
+  else
+    echo -e "\n${yellowColour}[!]${endColour}${redColour} La técnica introducida no existe${endColour}" 
+  helpPanel
+  fi
+fi
